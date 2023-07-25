531b3f2d3a75396abd662e7371bc8a42ceb46826
Ismail <ismail@Mini-de-Antoine.home>
2023-07-25 17:36:32 +0200

    refactor

--- models/Links.ts
+++ models/Links.ts
@@ -1,10 +1,8 @@
-import {typeEnum} from "~/typings/enums/type-enum";
 import {colorEnum} from "~/typings/enums/color-enum";
 
 export interface Links {
     id: number;
     target?: string;
-    type?: typeEnum;
     color?: colorEnum;
     url?: string;
 }
