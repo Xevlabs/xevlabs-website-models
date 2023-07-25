566a0b92d16bdb4e7233e699c101c7fbe9683161
Ismail <ismail@Mini-de-Antoine.home>
2023-07-25 12:14:55 +0200

    Strapi objects for navbar creation

--- models/Navbar.ts
+++ models/Navbar.ts
@@ -1,9 +1,7 @@
 import { Media } from './Media';
-import { Button } from './Button';
 
 export interface Navbar {
     id: number;
-    cta?: Button;
     logo?: { data: Media };
     title?: string;
 }
