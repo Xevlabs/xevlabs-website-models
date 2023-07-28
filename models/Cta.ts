4c1369114928baefda1729ca5abe0c99b1509055
Ismail <ismail@Mini-de-Antoine.home>
2023-07-28 17:38:20 +0200

    models

--- models/Cta.ts
+++ models/Cta.ts
@@ -3,8 +3,9 @@ import {colorEnum} from "~/typings/enums/color-enum";
 
 export interface Cta {
     id: number;
-    color?: colorEnum
+    color?: colorEnum;
     backgroundImage?: { data: Media };
     url?: string;
-    description?: string
+    description?: string;
+    hasArrow?: boolean;
 }
