877a46f99abb7d5ab0f46d972188df90b336bcc5
LARHCHIM ISMAIL <63025204+larhchim@users.noreply.github.com>
2023-08-01 19:55:41 +0100

    [RSI-50] Create the hero section (#4)
    * models necessary for Hero Section Creation
    
    * remove catchPhrase
    
    * change BUTTON to Link
    
    ---------
    
    Co-authored-by: Ismail <ismail@Mini-de-Antoine.home>

--- models/Button.ts
+++ models/Button.ts
@@ -2,10 +2,8 @@
 export interface Button {
     id: number;
     text?: string;
-    type?: 'BUTTON' | 'EXTERNAL_LINK' | 'INTERNAL_LINK';
+    type?: 'LINK';
     hasArrow?: boolean;
     color?: 'PRIMARY' | 'ALTERNATE';
     url?: string;
-    size?: 'SMALL' | 'REGULAR';
-    version?: 'PLAIN' | 'OUTLINE';
 }
