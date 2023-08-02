17109f832739097a2ca5abe79a643eb6ef27a4c7
Ismail <ismail@Mini-de-Antoine.home>
2023-08-02 17:42:55 +0200

    Adding necessary models for CatchPhraseCross

---
+++ models/CatchPhraseCross.ts
@@ -0,0 +1,8 @@
+import {colorEnum} from "~/typings/enums/color-enum";
+
+export interface CatchPhraseCross {
+    id: number;
+    title?: string
+    subtitles?: string
+    crossColor?: colorEnum
+}
