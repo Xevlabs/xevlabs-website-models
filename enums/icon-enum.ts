6b803ff754f8e26ff0852f0b61088f1fba509e8a
Noor Bookari <nour.boukari@gmail.com>
2023-08-10 12:10:29 +0100

    added iconEnum + updated Button model

---
+++ enums/icon-enum.ts
@@ -0,0 +1,5 @@
+export enum IconEnum {
+    ARROW = 'ARROW',
+    PLUS = 'PLUS',
+    NONE = 'NONE',
+}
--- models/Button.ts
+++ models/Button.ts
@@ -1,11 +1,11 @@
+import { IconEnum } from '../enums';
 import { colorEnum } from '../enums/color-enum';
 
 export interface Button {
     id: number;
     text?: string;
     type?: 'LINK' | 'CIRCLE';
-    hasArrow?: boolean;
-    hasPlus?: boolean;
+    icon?: IconEnum;
     color?: colorEnum;
     url?: string;
     overlayColor?: colorEnum;
