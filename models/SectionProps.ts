87833311424c5192386fbedf77d0892cc8571b83
Ismail <ismail@Mini-de-Antoine.home>
2023-08-18 17:48:24 +0200

    Having flat props

--- models/SectionProps.ts
+++ models/SectionProps.ts
@@ -1,10 +1,12 @@
+import { ColorEnum } from '../enums/color-enum';
 import { Border } from './Border';
-import { SectionCustomProperties } from './SectionCustomProperties';
 
 export interface SectionProps {
     title?: string;
     background?: string;
     border?: Border;
-    sectionCustomProp?: SectionCustomProperties;
+    headingTitleBorderColor?: ColorEnum;
+    headingTitle?: string;
+    headingCatchPhrase?: string;
     overflow?: boolean;
 }
--- models/TwoColsContent.ts
+++ models/TwoColsContent.ts
@@ -1,12 +1,13 @@
 import { ColorEnum } from '../enums/color-enum';
 import { Border } from './Border';
-import { SectionCustomProperties } from './SectionCustomProperties';
 
 export interface TwoColsContent {
     backgroundColor: ColorEnum;
     border?: Border;
     classes?: string[];
-    sectionProps?: SectionCustomProperties;
+    headingTitleBorderColor?: ColorEnum;
+    headingTitle?: string;
+    headingCatchPhrase?: string;
     description?: string;
     hasAsset?: boolean;
     hasDescriptionCards?: boolean;
