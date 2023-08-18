35068aca52ee726d933f45c50aa00e4af146f03a
Ismail <ismail@Mini-de-Antoine.home>
2023-08-18 10:59:50 +0200

    remove dark card background

--- models/DarkCard.ts
+++ models/DarkCard.ts
@@ -1,9 +1,7 @@
-import { ColorEnum } from '../enums/color-enum';
 import { Button } from './Button';
 
 export interface DarkCard {
     id: number;
     title: string;
     button: Button;
-    backgroundColor: ColorEnum;
 }
