8628c5f4d7a31404c9d84ff915d212d89f19d293
LARHCHIM ISMAIL <63025204+larhchim@users.noreply.github.com>
2023-08-23 09:39:38 +0200

    adding constant for button styles (#30)
    * adding constant for button styles
    
    * use more proper typing for ButtonColorStyle Constant
    
    * add undefined to color
    
    * create a type for my constant
    
    ---------
    
    Co-authored-by: Ismail <ismail@Mini-de-Antoine.home>

---
+++ constants/button-color-styles.const.ts
@@ -0,0 +1,56 @@
+import { ColorEnum } from '../enums/color-enum';
+import { ButtonColorStyles } from '../models/ButtonColorStyles';
+
+type ButtonColorStylesConstant = {
+    [key in ColorEnum]: ButtonColorStyles;
+};
+export const BUTTON_COLOR_STYLES_CONSTANT: ButtonColorStylesConstant = {
+    [ColorEnum.ALTERNATE]: {
+        backgroundColor: 'black',
+        hoverBackgroundColor: 'contrast',
+        textColor: 'white',
+        hoverTextColor: 'slate-200',
+    },
+    [ColorEnum.LIGHTGRAY]: {
+        backgroundColor: 'gray-300',
+        hoverBackgroundColor: 'gray-900',
+        textColor: 'black',
+        hoverTextColor: 'white',
+    },
+    [ColorEnum.DARKGRAY]: {
+        backgroundColor: 'darkgray-500',
+        hoverBackgroundColor: 'gray-900',
+        textColor: 'white',
+        hoverTextColor: 'white',
+    },
+    [ColorEnum.CONTRAST]: {
+        backgroundColor: 'contrast',
+        hoverBackgroundColor: 'gray-900',
+        textColor: 'white',
+        hoverTextColor: 'white',
+    },
+    [ColorEnum.GRAY]: {
+        backgroundColor: 'darkgray-500',
+        hoverBackgroundColor: 'darkgray-300',
+        textColor: 'white',
+        hoverTextColor: 'white',
+    },
+    [ColorEnum.DEFAULT]: {
+        backgroundColor: 'primary-500',
+        hoverBackgroundColor: 'darkgray-500',
+        textColor: 'white',
+        hoverTextColor: 'black',
+    },
+    [ColorEnum.PRIMARY]: {
+        backgroundColor: 'primary-500',
+        hoverBackgroundColor: 'primary-500',
+        textColor: 'white',
+        hoverTextColor: 'black',
+    },
+    [ColorEnum.WHITE]: {
+        backgroundColor: 'white',
+        hoverBackgroundColor: 'black',
+        textColor: 'black',
+        hoverTextColor: 'white',
+    },
+};
---
+++ models/ButtonColorStyles.ts
@@ -0,0 +1,6 @@
+export interface ButtonColorStyles {
+    backgroundColor: string;
+    hoverBackgroundColor: string;
+    textColor: string;
+    hoverTextColor: string;
+}
