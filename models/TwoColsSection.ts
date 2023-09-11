98df5eaf30f98c136adc0a3c8f7e05c064471f3b
Ismail <ismail@Mini-de-Antoine.home>
2023-09-11 17:40:06 +0200

    refactor

--- models/TwoColsSection.ts
+++ models/TwoColsSection.ts
@@ -1,7 +1,6 @@
 import { ColorEnum } from '../enums/color-enum';
 import { Border } from './Border';
 import { Column } from './Column';
-import { SectionHeading } from './SectionHeading';
 
 export interface TwoColsSection {
     id: number;
