6750665873751bcafa5cdef7fa623ab592506d7c
Ismail <ismail@Mini-de-Antoine.home>
2023-09-11 18:26:59 +0200

    refactor

--- models/SectionProps.ts
+++ models/SectionProps.ts
@@ -11,7 +11,6 @@ export interface SectionProps {
     overflow?: boolean;
     noPadding?: boolean;
     classList?: string[];
-    mobilePadding?: boolean;
     noContent?: boolean;
     noMobilePadding?: boolean;
 }
